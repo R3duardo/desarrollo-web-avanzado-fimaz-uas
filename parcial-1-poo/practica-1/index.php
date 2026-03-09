6ad615a954ded7b80ac54a9698196371acb04e12
R3duardo <multirandom12@gmail.com>
2026-03-09 09:37:50 -0700

    Instanciando la clase en el index

---
+++ parcial-1-poo/practica-1/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'Usuario.php';
+
+// Crear una instancia de la clase Usuario
+$usuario1 = new Usuario("Eduardo Montes de Oca Zatarain", "emozp@hotmail.com");
+
+// Mostrar los valores utilizando los métodos getters
+echo "Nombre del usuario: " . $usuario1->getNombre() . "\n";
+echo "Correo del usuario: " . $usuario1->getCorreo() . "\n";
+
+?>
