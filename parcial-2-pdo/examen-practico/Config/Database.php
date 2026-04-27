4d0933cf7e2e8928138b4fa0f8f82f08f49dbd96
R3duardo <multirandom12@gmail.com>
2026-04-26 18:07:54 -0700

    Agregando la conexion base de datos examen2

---
+++ parcial-2-pdo/examen-practico/Config/Database.php
@@ -0,0 +1,35 @@
+<?php
+// Alumno: Eduardo Montes de Oca Zatarain
+
+namespace Config;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $host = "localhost";
+    private $dbname = "compras";
+    private $username = "root";
+    private $password = "200511";
+    private $connection;
+
+    public function __construct()
+    {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
+            $this->connection = new PDO($dsn, $this->username, $this->password);
+
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
+?>
